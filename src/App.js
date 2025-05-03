63341c2762d7266551dc5fa19c241c9c20d21ade
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-05-03 19:05:49 +0200

    Redeploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AdminPage from "./pages/AdminPage";
-import SuperAdminPage from "./pages/SuperAdminPage"; // Add this import
+import SuperAdminPage from "./pages/SuperAdminPage";
 import SearchPage from "./pages/SearchPage";
 import Navbar from "./components/Navbar";
 import APIDocumentation from "./pages/APIDocumentation";
@@ -12,32 +11,31 @@ import PrivateRoute from "./PrivateRoute";
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-        <Navbar />
-        <div className="flex-1 w-full">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-              path="/admin"
-              element={
-                <PrivateRoute>
-                  <AdminPage />
-                </PrivateRoute>
-              }
-            />
-            {/* Add the new SuperAdmin route */}
-            <Route
-              path="/super-admin"
-              element={
-                <PrivateRoute>
-                  <SuperAdminPage />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/api-docs" element={<APIDocumentation />} />
-          </Routes>
-        </div>
+      <Navbar />
+      <div className="flex-1 w-full">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/super-admin"
+            element={
+              <PrivateRoute>
+                <SuperAdminPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/api-docs" element={<APIDocumentation />} />
+        </Routes>
       </div>
+    </div>
   );
 }
 
